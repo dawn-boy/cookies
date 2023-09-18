f6cd8e86d4f2af50578285169a4834024f9a02bd
dawn-boy <curiouslittleboy@pm.me>
2023-09-18 11:34:15 +0530

    Added Comments to the code.

--- fortuna.py
+++ fortuna.py
@@ -1,14 +1,15 @@
+# IMPORTS 
 import os
-from pprint import pprint
+
+# FORMATTING
 with open("My Clippings.txt",'r') as clipping:
     data = '==========\n'
     data += clipping.read()
-
 changes1 = data.splitlines()
 
 finalStr = ''
-
 onDisplay = True 
+
 for element in changes1:
     if element == '==========':
         onDisplay = False
@@ -18,7 +19,10 @@ for element in changes1:
         if element != '':
             finalStr += element
             finalStr += '\n%\n'
+
+# WRITING THE FORMATTED DATA
 with open("cookies",'w') as cookiesFile:
     cookiesFile.write(finalStr)
 
+# MAKING A .dat FILE SO FORTUNE CAN SELECT A HIGHLIGHT AT RANDOM
 os.system('strfile -c % cookies cookies.dat')
